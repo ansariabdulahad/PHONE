a72ca0a2e316271f9c69d591f5f65b2a74c12760
Sagar ghare <sagar.ghare@acc.ltd>
2023-06-28 13:16:10 +0530

    login code done

--- contactjust/src/assets/js/contact.js
+++ contactjust/src/assets/js/contact.js
@@ -2,12 +2,37 @@
 window.onload = function() {
     
     // GLOBAL VARIABLES
+    let nameBox = document.querySelector('#name-box');
     let nameEl = document.querySelector("#name");
+    let mobileBox = document.querySelector("#mobile-box");
     let mobileEl = document.querySelector('#mobile');
     let usernameEl = document.querySelector("#username");
     let passwordEl = document.querySelector("#password");
     let signupBtn = document.querySelector('#signup-btn');
     let notice = document.querySelector("#signup-notice");
+    let loginBtn = document.querySelector("#login-btn");
+
+    // CHECK LOCAL STORAGE IS EMPTY OR NOT AND PERFORM SOME ACTION
+    function checkSignup() {
+        if(localStorage.getItem('userData') != null) {
+            setTimeout(() => {
+                signupBtn.style.display = "none";
+                nameBox.style.display = "none";
+                mobileBox.style.display = "none";
+                loginBtn.style.display = "block";
+            }, 500);
+        }
+        else {
+            setTimeout(() => {
+                signupBtn.style.display = "block";
+                nameBox.style.display = "block";
+                mobileBox.style.display = "block";
+                loginBtn.style.display = "none";
+            }, 500);
+        }
+    }
+
+    checkSignup(); // calling...
 
     // SIGN UP BTN CODING
     signupBtn.onclick = function(e) {
@@ -30,6 +55,8 @@ window.onload = function() {
                 mobileEl.value = '';
                 usernameEl.value = '';
                 passwordEl.value = '';
+
+                checkSignup(); // calling...
             }, 500);
         }
         else {
@@ -39,5 +66,37 @@ window.onload = function() {
                 notice.style.display = "none";
             }, 3000);
         }
+    }
+
+    // LOGIN BTN CODING
+    loginBtn.onclick = function (e) { 
+        e.preventDefault();
+
+        if(localStorage.getItem("userData") != null) {
+            let userData = JSON.parse(localStorage.getItem("userData"));
+
+            if(usernameEl.value == userData.username) {
+                if(passwordEl.value == userData.password) {
+                    presentAlert("SUCCESS", "SUCCESS !");
+                }
+                else {
+                    presentAlert("Password is incorrect !", "Warning !");
+                }
+            }            
+            else {
+                presentAlert("Username not found !", "Warning !");
+            }
+        }        
+    }
+
+    // PRESENT ALERT FUNCTION CODING
+    async function presentAlert(message, header) {
+        let alert = document.createElement("ion-alert");
+        alert.header = header;
+        alert.message = message;
+        alert.buttons = ["OK"];
+
+        document.body.appendChild(alert);
+        await alert.present();
     }
 }
